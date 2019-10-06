0d4e5b1cf830087e3a4c24627cfa6f0e10c8418d
Ricardo Millanao <r.millanao02@ufromail.cl>
2019-10-06 19:54:22 -0300

    Create Ejemplo.java

---
+++ Curso/128-ForEach/Ejemplo.java
@@ -0,0 +1,15 @@
+//Es una forma de iterar sin contadores definidos
+//la principal deventaja es que no prodremos tener un control del arreglo
+public Ejemplo {
+
+  public static void main(String [] args){
+  
+  Persona [] personas = {new Persona("Juan"), new Persona("Karla")};
+  
+    for(Persona persona : persona) {
+     //Accedemos a las propiedad y/o metodos del objeto
+      
+      String nombre = person.getNombre();
+      Syste.out.println("Nombre persona: " +nombre;
+    }
+  }
