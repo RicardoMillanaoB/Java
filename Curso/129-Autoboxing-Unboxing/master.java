ea5acf64bde4d5c66e288af924274fbb707d0e28
Ricardo Millanao <r.millanao02@ufromail.cl>
2019-10-12 20:06:39 -0300

    Create master.java

---
+++ Curso/129-Autoboxing-Unboxing/master.java
@@ -0,0 +1,14 @@
+//Autoboxing proceso automatica de un tipo primitivo a un tipo object
+
+Integer enteroObj = 10;//tipo de clase envolvente, exister por cada tipo de primitivos
+Float flotanteObj = 15.2F; //asi se puede tratar con objeto
+Double dobleObj = 40.1;
+
+//Unboxing proceso de objeto a tipo primitivo
+
+int entero = enteroObj;
+float flotante = flotanteObj;
+double doble = dobleObj;
+
+//Clases envolvente desienden de la clase padre Number, 
+//Number contiene metodos propios
