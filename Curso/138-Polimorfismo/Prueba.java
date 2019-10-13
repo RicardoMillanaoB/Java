69f9a4b14e5039298b902584682b6377352d1f6f
Ricardo Millanao <r.millanao02@ufromail.cl>
2019-10-13 15:07:21 -0300

    Update Prueba.java

--- Curso/138-Polimorfismo/Prueba.java
+++ Curso/138-Polimorfismo/Prueba.java
@@ -6,13 +6,16 @@ public class Prueba {
         Empleado empleado1 = new Empleado("Esteban", 12000);
         Gerente gerente1 = new Gerente("Toman Echaurruen", 600000, "Gerencia general");
         System.out.println("Empleado1");
-        empleado1.obtenerDetalles();
+        imprimirDetalle(empleado1);
       
         System.out.println("Gerente");
         gerente1.obtenerDetalles();
-       
+       imprimirDetalle(gerente1);
 
     }
+public static void imprimirDetalle(Empleado emp){
+    System.out.println(emp.obtenerDetaller); // aqui ocurre el polimorfismo, al utilizar emp como un gerente
 
+}
 
 }
